a230482d140d82620ba07c8ea0efd8d975005312
yashika2406 <69670915+yashika2406@users.noreply.github.com>
2022-01-24 00:01:39 +0530

    Add files via upload

---
+++ Milestone1/10M_entries.cpp
@@ -0,0 +1,7 @@
+Maintain a minHeap of size 10
+Lets say you insert first 10 numbers (1,2,3,4,5,6,7,8,9,10) to the heap so that "1" will be the root
+Now next number is 11
+compare 11 with the root element
+if it is greater then remove the root and insert the number
+if it is smaller then do nothing
+In the end you will have largest 10 elements in the heap
